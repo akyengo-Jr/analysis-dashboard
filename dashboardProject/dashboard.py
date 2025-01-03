fe5ad6aa9f22a5484e4b95e19ba341386ce40bd3
Goodness O. Akyengo <154751971+akyengo-Jr@users.noreply.github.com>
2025-01-04 00:02:33 +0100

    Update dashboard.py

--- dashboardProject/dashboard.py
+++ dashboardProject/dashboard.py
@@ -16,9 +16,9 @@ st.set_page_config(layout='wide', page_title='Data Analytics Dashboard')
 st.title("Data Dashboard")
 
 # Load CSS file
-def load_css(file_path):
-    with open(file_path) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+#def load_css(file_path):
+ #   with open(file_path) as f:
+  #      st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 load_css("dashboardProject/style.css")
 

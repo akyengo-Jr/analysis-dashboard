2076456b7d205cdc07d2001e0147a2a74e19cfcf
Goodness O. Akyengo <154751971+akyengo-Jr@users.noreply.github.com>
2025-01-03 23:14:26 +0100

    Update dashboard.py

--- dashboardProject/dashboard.py
+++ dashboardProject/dashboard.py
@@ -142,7 +142,8 @@ elif date_management_option == "Convert to Proper Date Type" and st.sidebar.butt
     data_cleaned = convert_to_proper_date_type(data_cleaned, date_column)
     st.sidebar.success("Date type conversion successful!")
 else:
-    st.sidebar.error("Date task not performed")
+    st.sidebar.warning("This feature only works with dates")
+    
 # String cleaning options
 st.sidebar.header("String Cleaning")
 string_columns = st.sidebar.multiselect("Select columns to clean strings", data_cleaned.select_dtypes(include=['object']).columns)
